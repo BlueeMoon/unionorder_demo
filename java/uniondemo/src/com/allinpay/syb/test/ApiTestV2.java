5162f5757cc8857f6bcbf98aac31beba5528830a
dingjianwen <zzzowdp@163.com>
2017-03-22 10:15:27 +0800

    test1

--- java/uniondemo/src/com/allinpay/syb/test/ApiTestV2.java
+++ java/uniondemo/src/com/allinpay/syb/test/ApiTestV2.java
@@ -1,10 +1,10 @@
-package com.allinpay.syb.test;
+﻿package com.allinpay.syb.test;
 
 import java.util.Map;
 
 import com.allinpay.syb.lib.SybPayService;
 
-
+vdfgddhbgfnbgfnbgn
 public class ApiTestV2 {
 	public static void main(String[] args) throws Exception{
 		testPay();
